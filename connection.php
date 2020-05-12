4e75406ade7aeab16a0c639364e1fa4ca50631cf
Jaimin25 <65119631+Jaimin25@users.noreply.github.com>
2020-05-12 10:25:41 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,25 @@
+<?php
+
+//Your server name, it will be same for all 000webhost accounts
+$servername = "your_servername";
+
+//Your DB username
+$username = "your_database_username";
+
+//Your DB password
+$password = "your_database_password";
+
+//Your DB name, required if you have two DB and want to connect to a specific one
+$dbname = "your_database_name";
+
+//Connect to MySQL
+$mysql = mysqli_connect($servername, $username, $password, $dbname);
+
+//Check connection
+if($mysql->connect_error){
+echo "Connection failed: ". connect_error;
+} else {
+echo "Connected successfully";
+}
+
+?>
